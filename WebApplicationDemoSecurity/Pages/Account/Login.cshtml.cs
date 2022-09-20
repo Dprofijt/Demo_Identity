57ea90099bdf20002a1217beb03bd833ae7ad8c2
damian <damian@profijt.eu>
2022-09-20 14:04:30 +0200

    Add project files.

---
+++ WebApplicationDemoSecurity/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplicationDemoSecurity.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty] public Credential Credential { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            // Validate Credential
+            if (Credential.UserName == "admin" && Credential.Password == "password")
+            {
+                //Creating the security context
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, " admin "),
+                    new Claim(ClaimTypes.Email, " admin@mywebsite.com "),
+                    new Claim("Department", "HR"), 
+                    new Claim("Admin", "true"),
+                    new Claim("Manager", "true"),
+                    new Claim("EmploymentDate", "2021-05-01")
+                };
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+                
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+
+                return RedirectToPage("/Index");
+            }
+
+            return Page();
+        }
+    }
+
+    public class Credential
+    {
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        
+    }
+}
